fed514a417506014fdf0da84baab27d2c8a91f1e
Administrator <benoit.routier@semifir.com>
2022-05-24 09:05:49 +0200

    checkpoint(j2 17:00)

--- src/Controller/FilmsApiController.php
+++ src/Controller/FilmsApiController.php
@@ -5,41 +5,107 @@ namespace App\Controller;
 use App\Entity\Film;
 use App\Repository\FilmRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\{Request, Response};
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Translation\Exception\NotFoundResourceException;
 
 #[Route('/api/films')]
 class FilmsApiController extends AbstractController
 {
-    #[Route('/', name: 'api_films', methods:["GET"])]
+    #[Route('', name: 'api_films', methods: ["GET"])]
     public function index(FilmRepository $filmRepository): Response
     {
         $films = $filmRepository->findAll();
         return $this->json($films);
     }
 
-    #[Route('/', name: 'api_films_create', methods:["POST"])]
-    public function create(Film $film): Response
+    #[Route('', name: 'api_films_create', methods: ["POST"])]
+    public function create(Request $request, FilmRepository $filmRepository): Response
     {
+        $data = json_decode($request->getContent(), true);
+        $film = new Film();
+        $film->setTitre($data['titre']);
+        $film->setDateSortie(new \DateTime($data['dateSortie']));
+        $film->setProducteur($data['producteur']);
+
+        // Opérateur null coalesce ??
+        $film->setDescription($data["description"] ?? null);
+        // avec ternaire
+        // $film->setDescription($request->get("description") ? $request->get("description") : null);
+
+        $filmRepository->add($film, true);
+
         return $this->json($film);
     }
 
-    #[Route('/{id}', name: 'api_films_show', methods:["GET"])]
-    public function show(int $id): Response
+    #[Route('/{id}', name: 'api_films_show', methods: ["GET"])]
+    public function show(int $id, FilmRepository $filmRepository): Response
     {
-        return $this->json($this->films[$id]);
+        $film = $filmRepository->find($id);
+        if($film){
+            return $this->json($film);
+        }
+        return $this->json(null, 404);
     }
 
-    #[Route('/{id}', name: 'api_films_update', methods:["PUT"])]
-    public function update(Film $film): Response
+    #[Route('/{id}', name: 'api_films_update', methods: ["PUT"])]
+    public function update($id, Request $request, FilmRepository $filmRepository): Response
     {
+        // extraction du json de la requête
+        $data = json_decode($request->getContent(), true);
+
+        // récup du film à modifier
+        $film = $filmRepository->find($id);
+
+        if ($film) {
+            // changements à appliquer
+            $film->setTitre($data['titre']);
+            $film->setProducteur($data['producteur']);
+            $film->setDateSortie(new \DateTime($data['dateSortie']));
+            $film->setDescription($data['description'] ?? $film->getDescription());
+        } else {
+            // throw $this->createNotFoundException();
+            return $this->json(null, 404);
+        }
+
+        $filmRepository->add($film, true);
+        // $filmRepository->update($film, true);
+
         return $this->json($film);
     }
 
-    #[Route('/{id}', name: 'api_films_delete', methods:["DELETE"])]
-    public function delete(int $id): Response
+    #[Route('/{id}', name: 'api_films_patch', methods: ["PATCH"])]
+    public function shallow_update($id, Request $request, FilmRepository $filmRepository): Response
     {
-        return $this->json($this->films[$id]);
+        // extraction du json de la requête
+        $data = json_decode($request->getContent(), true);
+
+        // récup du film à modifier
+        $film = $filmRepository->find($id);
+
+        if ($film) {
+            $film->setTitre($data['titre'] ?? $film->getTitre());
+            $film->setProducteur($data['producteur'] ?? $film->getProducteur());
+            $film->setDateSortie(isset($data['dateSortie']) ? new \DateTime($data['dateSortie']) : $film->getDateSortie());
+            $film->setDescription($data['description'] ?? $film->getDescription());
+        } else {
+            return $this->json(null, 404);
+        }
+
+        $filmRepository->add($film, true);
+        // $filmRepository->update($film, true);
+
+        return $this->json($film);
     }
 
+    #[Route('/{id}', name: 'api_films_delete', methods: ["DELETE"])]
+    public function delete(int $id, FilmRepository $filmRepository): Response
+    {
+        $film = $filmRepository->find($id);
+        if($film){
+            $filmRepository->remove($film, true);
+            return $this->json(null, 204);
+        }
+        return $this->json(null, 404);
+    }
 }
---
+++ src/Form/FilmType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Film;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FilmType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('date_sortie',  DateType::class, [
+                'widget' => 'text',
+            ])
+            ->add('producteur')
+            ->add('description')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Film::class,
+        ]);
+    }
+}
